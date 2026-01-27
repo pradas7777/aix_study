f458354db48f45c809a00c9f634c547e60bb7f1f
pradas7777 <pradas7777@gmail.com>
2026-01-27 17:33:10 +0900

    베타 V2

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ async def board_list(
     
     posts = db.query(models.Post).filter(models.Post.type == post_type).order_by(models.Post.created_at.desc()).all()
     
-    titles = {"summary": "수업 요약", "qna": "질문 답변", "lounge": "자유 게시판", "study" : "그룹 스터디", "suggestion": "기능 건의"}
+    titles = {"summary": "수업 요약", "qna": "질문 답변", "lounge": "자유 게시판", "study" : "그룹 스터디", "suggestion": "홈페이지 기능 건의","updates": "홈페이지 업데이트 내역"}
     board_title = titles.get(post_type, "게시판")
 
     return templates.TemplateResponse("list.html", {
@@ -169,7 +169,7 @@ async def write_page(
     visitor_uuid: Optional[str] = Cookie(None)
 ):
     visitor, _ = get_or_create_visitor(db, visitor_uuid)
-    titles = {"summary": "수업 요약", "qna": "질문 답변", "lounge": "자유 게시판", "study" : "그룹 스터디", "suggestion": "기능 건의"}
+    titles = {"summary": "수업 요약", "qna": "질문 답변", "lounge": "자유 게시판", "study" : "그룹 스터디", "suggestion": "홈페이지 기능 건의","updates": "홈페이지 업데이트 내역"}
     board_title = titles.get(post_type, "게시판")
     
     # 기존의 글쓰기 폼이 들어있는 board.html을 보여줍니다.
